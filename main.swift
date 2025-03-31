eb20096d856ceb314732086fff12ca6946fd11d3
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2025-03-31 17:22:31 -0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,17 @@
+import Foundation
+import AVFoundation
+import Darwin
+
+let board = Board()
+var fps = 0.0
+var intervalo: Double
+var score = 0
+var gameOverVerify: Int = 0
+var endGameVerify: Int = 0
+var dificuldade: Int
+var elapsedTimeMusic: TimeInterval
+var audioPlayer: AVAudioPlayer?
+
+startMenu()
+
+// *Mudar o caminho dos arquivos das músicas no aba Game dentro das funções playMusic()*
